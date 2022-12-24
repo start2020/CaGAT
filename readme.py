63e0474ba9af4b6e3b3f727ff56933e30ce90ca5
kakazai <40651244+start2020@users.noreply.github.com>
2022-12-24 17:11:50 +0800

    Update readme.py

--- readme.py
+++ readme.py
@@ -1,1 +1,6 @@
 This is an introduction of LAD-CaGAT
+Part of the code is from https://github.com/squareRoot3/Rethinking-Anomaly-Detection
+The Amazon datasets will be automatically downloaded from the Internet.
+
+How to run
+python main.py 
